24a5f0c32a1b0a030c3a8d0baea672e083f0d2dd
josh231101 <wiijosue333@gmail.com>
2020-07-08 14:40:20 -0500

    request targeting specif route

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ const articleSchema = mongoose.Schema({
 })
 const Article = mongoose.model("Article",articleSchema)
 
+////////////////////////////////////////////////////////////////////////////////// REQUEST TARGETING ALL ARTICLES //////////////////////////
 app.route("/articles")
     .get(function(req, res){
       Article.find(function(e,foundArticles){
         if(!e){
-          console.log(foundArticles);
           res.send(foundArticles)
         }
         else{res.send(e)}
@@ -36,7 +36,7 @@ app.route("/articles")
         if(!e){
           res.send("Successfully added!")
         }else{
-          res.send("UPS! Somethin went wrong")
+          res.send("UPS! Something went wrong")
         }
       });
       console.log(req.body.title);
@@ -59,6 +59,61 @@ app.post("/articles",)
 
 //HTTP REQUEST: DELETE
 app.delete("/articles",)*/
+
+////////////////////////////////////////////////////////////////////////////////// REQUEST TARGETING A SPECIFIC ARTICLE//////////////////////////
+
+app.route("/articles/:articleTitle")
+.get(function(req,res){
+  Article.findOne({title: req.params.articleTitle},function(e,foundArticle){
+    if(foundArticle){
+      res.send(foundArticle)
+    }else{
+      res.send("No artciles matching that title was found.")
+
+    }
+  })
+})
+.put(function(req,res){
+  //Replace a particular document inside our articles collection
+  Article.update(
+    //CONDITION
+    {title : req.params.articleTitle},
+    //UPDATE WE WANT TO MAKE
+    {title : req.body.title, content: req.body.content},
+    //CHANGE ALL THE DOCUMENT, if user doesn't send title or content they will be deleted
+    {overwrite : true},
+    function(err){
+    if(!err){
+      res.send("Succesfully updated article")
+    }else{res.send("Something went wrong");}
+  })
+
+})
+.patch(function(req,res){
+  //If we dont want to overwrite everything.UPDATE and specif thing
+
+  //req.body = { title : "TEST",content : "TEST"}
+  //if no title looks like req.body = {content : "TEST"}
+
+  Article.update(
+    {title : req.params.articleTitle},
+    //ONLY UPDATE THE VALUES COMING FROM THE req.body
+    {$set : req.body},
+    function(e){
+      if(!e){res.send("UPDATED")}else{res.send("Something went wrong")}
+    }
+  )
+})
+.delete(function(req,res){
+  Article.deleteOne({title : req.params.articleTitle},function(e){
+    if(!e){
+      res.send("DOCUMENT DELETED")
+    }else{
+      res.send("UPS! Soemthing went wrong")
+    }
+  })
+
+});
 
 app.listen(8080,function(){
   console.log("Page running of port 8080");
